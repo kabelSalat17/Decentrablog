4c0be340e1fea6169dcfd6da101f11942a68e5f6
Matteo Brandolino <matteo.brandolino@gmail.com>
2021-01-18 19:42:06 +0100

    truffle-config modified

--- truffle-config.js
+++ truffle-config.js
@@ -9,6 +9,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
   },
+  //Where contracts and abis are saved
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
